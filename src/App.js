b69086034801cab7a3d2b5976a152ebd49e4598d
Shriniket Sarkar <shriniket.sarkar@cerner.com>
2021-03-10 11:12:41 -0600

    fix: Fixed existing test case but moving the BrowserRouter in the correct place.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import BookSearch from './search/BookSearch';
 import Bookcase from './main-page/Bookcase';
 import './App.css';
 
 const BooksApp = () => {
   return (
-    <div className="app">
-      <Route exact path='/' render={() => (
-        <div className="list-books">
-          <div className="list-books-title">
-            <h1>MyReads</h1>
+    <BrowserRouter>
+      <div className="app">
+        <Route exact path='/' render={() => (
+          <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <Bookcase />
+            <div className="open-search">
+              <Link to='/search'>
+                <button>Add a book</button>
+              </Link>
+            </div>
           </div>
-          <Bookcase />
-          <div className="open-search">
-            <Link to='/search'>
-              <button>Add a book</button>
-            </Link>
-          </div>
-        </div>
-      )} />
-      <Route path='/search' component={BookSearch} />
-    </div>
+        )} />
+        <Route path='/search' component={BookSearch} />
+      </div>
+    </BrowserRouter>
   )
 }
 
